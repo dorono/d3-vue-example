d70a0ff0e654c839ae3b900b981e75555d73e2c4
Doron Orenstein <jazziseverywhere@gmail.com>
2020-03-18 19:24:48 -0500

    added state management with vuex

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Home from '../views/Home.vue';
-import Login from '../views/Login.vue';
+import Counting from '../views/Counting.vue';
 
 Vue.use(Router);
 
@@ -13,9 +13,9 @@ export default new Router({
       component: Home
     },
     {
-      path: '/login',
-      name: 'login',
-      component: Login
+      path: '/counting',
+      name: 'counting',
+      component: Counting
     },
   ]
 });
---
+++ src/views/Counting.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="counting">
+    Count: {{count}}
+    <hr>
+    <button v-on:click="increment">INCREMENT</button> | <button v-on:click="decrement">DECREMENT</button>
+  </div>
+</template>
+
+<script>
+import store from '../store';
+
+export default {
+  name: "counting",
+  computed: {
+    count() {
+      return store.state.count;
+    }
+  },
+  methods: {
+    increment() {
+      store.commit('increment');
+    },
+    decrement() {
+      store.commit('decrement');
+    }
+  }
+};
+</script>
